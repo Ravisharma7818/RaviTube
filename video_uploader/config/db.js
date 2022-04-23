f9ed382c4a68c780b338b051c74db7cd0a980254
ravi2018cs115abesit@gmail.com <ravi2018cs115abesit@gmail.com>
2022-04-23 03:58:50 -0700

    Add Online Db

--- video_uploader/config/db.js
+++ video_uploader/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/VideoApp", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
--- video_uploader/index.js
+++ video_uploader/index.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const dotenv = require('dotenv');
 const app = express();
-
+dotenv.config();
 // Passport Config
 require('./config/passport-config')
 
